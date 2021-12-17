0b225e0916e8daa2b45d81f701cb2ea22f36ebc1
tai271 <phanductai920510@gmail.com>
2021-12-17 08:47:15 +0700

    first commit

---
+++ assets/js/javascript.js
@@ -0,0 +1,37 @@
+//disable smooth scrolling (tắt smooth khi dùng con lăn của con chuột)
+var x = window.scrollX;
+var y = window.scrollY;
+window.onscroll = function () {
+  window.scrollTo(x, y);
+};
+
+
+// opening/closing on menu icon
+var header = document.getElementById("header");
+var mobileMenu = document.getElementById("mobileMenu");
+var currentHeaderHeight = header.clientHeight;
+
+mobileMenu.onclick = function () {
+  var isClose = header.clientHeight === currentHeaderHeight;
+  if (isClose) {
+    header.style.height = "fit-content";
+  } else {
+    header.style.height = null;
+  }
+};
+
+// opening/closing on menu items
+var menuItems = document.querySelectorAll('#nav li a[href*="#"]');
+
+for (var i = 0; i < menuItems.length; i++) {
+  var item = menuItems[i];
+  item.onclick = function (event) {
+    var isParentMenu = this.nextElementSibling && this.nextElementSibling.classList.contains('subnav');
+    
+    if (isParentMenu) {
+      event.preventDefault();
+    } else {
+      header.style.height = null;
+    }
+  }
+};
